694c2a1913949ae9fec63c496fb614080e810788
Navin-MS-data <navinms0111@gmail.com>
2026-04-07 12:37:17 +0530

    explring get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 import express from 'express';
 
 const app = express();
-
-app.use('/', (req, res, next) => {
-  res.send('Namaste Navin');
+//This will handle GET requests to /user
+app.get('/user', (req, res, next) => {
+    res.send({ firstName: 'Navin', lastName: 'Kumar' });
 });
 
-app.use('/hello', (req, res, next) => {
-  res.send('Hello hello Hello');
+//This will handle POST requests to /user
+app.post('/user', (req, res, next) => {
+    res.send('Data saved successfully');
 });
 
+app.delete('/user', (req, res) => {
+    res.send('User deleted successfully');
+});
+
+//this will match all the HTTP method api calls to /test
 app.use('/test', (req, res, next) => {
-  res.send('Hello from the server!');
+    res.send('Hello from the server!');
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on PORT 3000');
+    console.log('Server is running on PORT 3000');
 });
