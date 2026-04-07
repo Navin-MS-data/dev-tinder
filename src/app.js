7ff58d8780ab42670d045b99512c762b1521a514
Navin-MS-data <navinms0111@gmail.com>
2026-04-07 11:46:56 +0530

    style: format code for consistency and readability

--- src/app.js
+++ src/app.js
@@ -3,17 +3,17 @@ import express from 'express';
 const app = express();
 
 app.use('/', (req, res, next) => {
-    res.send('Namaste Navin');
+  res.send('Namaste Navin');
 });
 
 app.use('/hello', (req, res, next) => {
-    res.send('Hello hello Hello');
+  res.send('Hello hello Hello');
 });
 
 app.use('/test', (req, res, next) => {
-    res.send('Hello from the server!');
+  res.send('Hello from the server!');
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on PORT 3000');
+  console.log('Server is running on PORT 3000');
 });
