eaaee6bc2f5a8c654e53b6cc1db93bd315b2cf15
Navin-MS-data <navinms0111@gmail.com>
2026-04-09 16:27:56 +0530

    feat: add user retrieval and feed API endpoints
    - Implemented GET /user to fetch a user by email
    - Added GET /feed to retrieve a list of all users
    - Improved error handling for user-related operations

--- src/app.js
+++ src/app.js
@@ -7,23 +7,46 @@ const app = express();
 app.use(express.json());
 
 app.post('/signup', async (req, res) => {
-    //creating a new instance of User model
-    const user = new User(req.body);
-    try {
-        await user.save();
-        res.send('User added successfully');
-    } catch (err) {
-        res.status(400).send('Error adding user', err.message);
+  //creating a new instance of User model
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send('User added successfully');
+  } catch (err) {
+    res.status(400).send('Error adding user', err.message);
+  }
+});
+
+app.get('/user', async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const users = await User.findOne({ email: userEmail });
+    if (users.length === 0) {
+      res.status(404).send('User not found');
     }
+    res.send(users);
+  } catch (error) {
+    res.status(400).send('Error getting user', error.message);
+  }
+});
+
+//Feed API - GET /feed - get a list of users from the database
+app.get('/feed', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send('Error getting feed', error.message);
+  }
 });
 
 connectDB()
-    .then(() => {
-        console.log('database connection established');
-        app.listen(3000, () => {
-            console.log('Server is running on PORT 3000');
-        });
-    })
-    .catch((err) => {
-        console.error('database connection failed');
+  .then(() => {
+    console.log('database connection established');
+    app.listen(3000, () => {
+      console.log('Server is running on PORT 3000');
     });
+  })
+  .catch((err) => {
+    console.error('database connection failed');
+  });
